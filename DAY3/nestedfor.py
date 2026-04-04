6a31f60eb01aafba5e3eac2933cdeba19f87e220
Yash <yashpatkar4249@gmail.com>
2026-04-04 12:52:48 +0530

    Add new nested loop examples and patterns in Python; include comments for clarity

--- DAY3/nestedfor.py
+++ DAY3/nestedfor.py
@@ -1,6 +1,52 @@
-for i in range(1,4):
-    for j in range(1,4):
-        print(i, end = " " )
-    print() # for new line after each iteration of outer loop
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i, end = " " )
+#     print() # for new line after each iteration of outer loop
     
+
+# n = int(input("Enter the number of rows: "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j, end = " ")
+#     print()
+
+
+# n = int(input("Enter the number of rows: "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(chr(64+j), end = " ")
+#     print()
     
+# n = int(input("Enter the number of rows: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print("*", end = " ")
+#     print()
+
+# n = int(input("Enter the number of rows: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(chr(64+j), end = " ")
+#     print()
+
+# import time
+# n = int(input("Enter the number of rows: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         time.sleep(2)
+#         print(n+1-i, end = " ")
+#     print()
+
+# import time
+# n= int(input("Enter the number of rows: "))
+# for i in range(1,n+1):
+#     print(" "*(n-i), end = "")
+#     for j in range(1,i+1):
+#         time.sleep(0.1)
+#         print("* ", end = "")
+#     print()
+    
+# n = int(input("Enter the number of rows: "))
+
+# for i in range(n, 0, -1):
+#     print(" " * (n - i) + "*" * (2 * i - 1))
